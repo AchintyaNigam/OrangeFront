e738f1e5de9fdadde0fe4b64558cb49ce385552a
AchintyaNigam <achintyanigam2@gmail.com>
2024-08-09 22:34:42 +0530

    home page snap change

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
   
     <div className="bg-white">
-      <Image src="/background.png" className="object-fill h-screen w-screen" width={1920} height={1080} alt="Background" />
-      <div className="flex-1 md:flex">
+      <Image src="/background.png" className="object-fill h-screen w-screen scroll-none" width={1920} height={1080} alt="Background" />
+      <div className="flex-1">
         <p>catSpector</p>
       </div>
     </div>
